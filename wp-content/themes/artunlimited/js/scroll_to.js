d2b2094c20032f7902af33386ca51ed93b4393e1
unknown <diego@brasa.art.br>
2013-09-30 17:33:04 -0300

    ajuste 1px do scroll to

--- wp-content/themes/artunlimited/js/scroll_to.js
+++ wp-content/themes/artunlimited/js/scroll_to.js
@@ -2,6 +2,6 @@
 // <a href="javascript:scroll_to('#nav-quem-somos');">Quem Somos</a>
 function scroll_to (target) {
 	jQuery('html, body').animate({
-		scrollTop: jQuery(target).offset().top - 100
+		scrollTop: jQuery(target).offset().top - 99
 	}, 500);
 }
