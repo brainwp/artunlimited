a30a37cebacd3910ea81e6a5ce86cce36c88f449
Everaldo Matias <matiaseveraldo@gmail.com>
2013-10-21 21:32:28 -0200

    Melhhorando slider da single-portfolio.php.

--- wp-content/themes/artunlimited/js/caroufredsel_pre.js
+++ wp-content/themes/artunlimited/js/caroufredsel_pre.js
@@ -4,17 +4,8 @@ jQuery(function() {
 	width: "100%",
     align: 'center',
     responsive: true,
-    items: {visible: 3, minimum: 2, start: -1},
-    scroll: { items: 1, pauseOnHover: true, duration: 1000, timeoutDuration: 3000 ,onBefore: function(){
-            jQuery('ul#carousel li')
-                .animate({opacity:0.5}, 250);
-        },
-        onAfter: function(){
-            jQuery('ul#carousel li')
-                .filter(':eq(1)')
-                .animate({opacity:1}, 250);
-        }
-    },
+    items: { width: "variable", visible: 2},
+    scroll: { items: 1, pauseOnHover: true, duration: 1000, timeoutDuration: 3000 },
     auto: { play: true },
 	prev: '#prev2',
 	next: '#next2',
