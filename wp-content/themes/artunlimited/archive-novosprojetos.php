c1d36d610db7689f3007d8693ec475cfc5155cb3
Your Name <diego@brasa.art.br>
2014-10-23 00:35:43 -0200

    novas imagens de abas, exclusao de inuteis e estilos de novos projetos

--- wp-content/themes/artunlimited/archive-novosprojetos.php
+++ wp-content/themes/artunlimited/archive-novosprojetos.php
@@ -1,42 +1,18 @@
-<?php get_header( 'portfolio' ); ?>
+<?php get_header( 'novosprojetos' ); ?>
 
 		<div id="content-interno" class="site-content" role="main">
 		
 		<div class="archive-portfolio">
 			
-			        <div class="header-categories">
-                            <ul class="separated-list menu">
-                                <?php
-                                  $myterms = get_terms( 'tipo' );
-									$conta = 0;
-                                  foreach($myterms as $term){
-                                    $root_url = get_bloginfo('url');
-                                    $term_taxonomy = $term->taxonomy;
-                                    $term_slug = $term->slug;
-                                    $term_name = $term->name;
-                                    $link = $root_url.'/'.$term_taxonomy.'/'.$term_slug;
-                                    /*$output .= "<option value='".$link."'>".$term_name."</option>";*/
-																		
-								 if ($conta < 1){
-									$i = "<li class=\"primeiro-li\"><a href=";
-								} else {
-									$i = "<li><a href=";
-								}
-								echo $i;
-                                  echo $link . ">" . $term_name;
-                                  echo "</a></li>";
-								$conta++;
-                                  }
-                                ?>
-                            </ul>
-                            <div id="busca-aba">
+			        <div class="header-portfolio">
+                        <div id="busca-aba" class="portfolio">
                             <div id="lupa-aba"></div>
                             <form id="searchform" action="<?php bloginfo('url'); ?>/" method="get">
-                            <input class="inlineSearch" type="text" name="s" value="busca" onblur="if (this.value == '') {this.value = 'busca';}" onfocus="if (this.value == 'busca') {this.value = '';}" />
-                            <input type="hidden" name="post_type" value="novosprojetos" />
-                            <!-- <input class="inlineSubmit" id="searchsubmit" type="submit" alt="Search" value="Buscar" /> -->
+								<input class="inlineSearch" type="text" name="s" value="busca" onblur="if (this.value == '') {this.value = 'busca';}" onfocus="if (this.value == 'busca') {this.value = '';}" />
+								<input type="hidden" name="post_type" value="portfolio" />
+								<!-- <input class="inlineSubmit" id="searchsubmit" type="submit" alt="Search" value="Buscar" /> -->
                             </form>
-                            </div><!-- #busca-aba -->
+                        </div><!-- #busca-aba -->
                     </div><!-- .header-categories -->
 			
 						<?php
@@ -61,7 +37,16 @@
 							</div><!-- .thumb-cada-projeto -->
 						
 							<div class="rodape-cada-projeto">
-								<h3><a class="titulo-resumo" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								<h3><a class="titulo-resumo" href="<?php the_permalink(); ?>"><?php the_title(); ?><br />
+								<?php
+								// Pega os dados e salva em variáveis
+								$metaportfolio_2alinhatitulo = get_post_meta($post->ID,'metaportfolio_2alinhatitulo',TRUE);
+								?>
+								<?php if (empty($metaportfolio_2alinhatitulo)) {
+								} else { ?>
+									  <?php echo $metaportfolio_2alinhatitulo; ?>
+								<?php }	?>
+								</a></h3>
 								<span class="data-cada-post"><?php the_time( 'Y' ); ?></span>
 							</div><!-- .rodape-cada-projeto -->
 						</div><!-- .cada-projeto -->
@@ -73,6 +58,7 @@
 						?>
 
 		</div> <!-- .archive-portfolio -->
+
 
 		</div><!-- #content -->
   
