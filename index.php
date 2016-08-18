89c32532f68b7bd1a2c791c372e3603c6a6dd58f
Diego Rojas <diego@brasa.art.br>
2016-08-18 20:04:43 -0300

    apagando arquivos do core wp e movendo o repo para tema

--- index.php
+++ index.php
@@ -1,17 +1,91 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package artunlimited
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header( 'interno' ); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<div class="altura-header"></div>
+
+<div id="content-index" class="site-content" role="main">
+
+	<div id="primary" class="content">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<!-- <div class="full-thumb">
+						<?php // the_post_thumbnail (); ?>
+					</div> -->
+					
+                    <header class="entry-header-noticias">
+
+						<h1 class="titulo-noticias"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+							<?php if ( 'post' == get_post_type() ) : ?>
+
+						<div class="entry-meta">
+
+							<?php the_time( get_option( 'date_format' ) ); ?> 
+
+						</div><!-- .entry-meta -->
+
+						<?php endif; ?>
+
+					</header><!-- .entry-header -->
+
+					<div class="clear"></div>
+						
+
+					<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+
+					<div class="entry-summary">
+
+						<?php the_excerpt(); ?>
+
+					</div><!-- .entry-summary -->
+
+					<?php else : ?>
+
+					<div class="entry-content">
+
+						<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'artunlimited' ) ); ?>
+
+					</div><!-- .entry-content -->
+
+					<?php endif; ?>
+
+					<footer class="entry-meta">
+						<?php edit_post_link( __( 'Edit', 'artunlimited' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-meta -->
+
+				</article><!-- #post-## -->
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'no-results', 'index' ); ?>
+
+		<?php endif; ?>
+
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+
+</div><!-- #content -->
+
+<?php get_footer(); ?>
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Repositório de todo o projeto para o Tema para a Art Unlimited
-
-Desenvolvido com o undescores da Auttomatic esse tema têm sido um dos maiores desafios atuais da Brasa.
