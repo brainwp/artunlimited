14db9eb74c22c6a2cd3553370bcfc4b3cc7e7cb6
Diego Rojas <diego@brasa.art.br>
2016-08-18 21:34:14 -0300

    melhoria na autura do header e correção no scroll to

--- js/scroll_to.js
+++ js/scroll_to.js
@@ -2,6 +2,6 @@
 // <a href="javascript:scroll_to('#nav-quem-somos');">Quem Somos</a>
 function scroll_to (target) {
 	jQuery('html, body').animate({
-		scrollTop: jQuery(target).offset().top - 99
+		scrollTop: jQuery(target).offset().top - 84
 	}, 500);
 }
