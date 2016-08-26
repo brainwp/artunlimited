2e3874ff2a93afa2109984f4122ed1e3dacbfc57
Diego Rojas <diego@brasa.art.br>
2016-08-26 12:40:04 -0300

    fix bandeiras em todos os templates, rodape com redes sociais no mobile

--- header-interno.php
+++ header-interno.php
@@ -142,13 +142,11 @@ if ( is_user_logged_in() ) {
 		 <div class="area-3-header">
         
         <div id="linguas">
-        	<div id="en">
-            	<a class="a-linguas" href=""></a>
-		    </div><!-- #en -->
-            
-            <div id="pt">
-				<a class="a-linguas" href=""></a>	
-		    </div><!-- #pt -->
+            <?php 	
+            	if (function_exists('dynamic_sidebar')) {
+					dynamic_sidebar('Widget no menu');
+				} 
+			?>
         </div><!-- #linguas -->
         </div><!-- .area-3-header -->
 		
